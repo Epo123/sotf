3635faa4dac8078271e992ce23da5495aa72ed10
JJStoker <jeroen229@gmail.com>
2014-01-28 09:38:04 +0100

    cleanup

--- app/controllers/APIShoppingCartController.php
+++ app/controllers/APIShoppingCartController.php
@@ -39,7 +39,6 @@ class APIShoppingCartController extends \BaseController {
         $cashregister = CashRegister::where("code", "=", $code)->get();
 
         if (Auth::attempt(array('email' => Input::get('email'), 'password' => Input::get('password')))){
-
             $productAndAmount = json_decode(Input::get("products"));
             $productInformation = array();
 
@@ -55,60 +54,24 @@ class APIShoppingCartController extends \BaseController {
             }
 
             if($productInformation != null) {
-
                 $userInformation = array(
                     "userid" => Auth::user()->id,
                     "surname" => Auth::user()->surname,
                     "gender" => Auth::user()->gender,
                 );
-
                 $cashRegister = json_encode(array(
                     "userInformation" => $userInformation,
                     "products" => $productInformation
                 ));
-
-//            return $cashRegister;
-
                 $service_port = 8818;
-//                getservbyport(8818, 'tcp');
-
                 $address = gethostbyaddr('145.37.87.36');
-
                 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-
-                if ($socket === false) {
-                    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
-                } else {
-//                    echo "OK.\n";
-                }
-
-//                echo "Attempting to connect to '$address' on port '$service_port'...";
                 $result = socket_connect($socket, $address, $service_port);
-                if ($result === false) {
-                    echo "socket_connect() failed.\nReason: ($result) " . socket_strerror(socket_last_error($socket)) . "\n";
-                } else {
-//                    echo "OK.\n";
-                }
-
                 $in = $cashRegister;
                 $out = '';
-
-//                echo "Sending HTTP HEAD request...";
                 socket_write($socket, $in, strlen($in));
-//                echo "OK.\n";
-
-//            echo "Reading response:\n\n";
-//            while ($out = socket_read($socket, 2048)) {
-//                echo $out;
-//            }
-
-//                echo "Closing socket...";
                 socket_close($socket);
-//                echo "OK.\n\n";
-
                 Auth::logout();
-
-                // send to kassa
                 return Response::json(array('status' => '1'));
             }
             return Response::json(array('status' => '0'));
